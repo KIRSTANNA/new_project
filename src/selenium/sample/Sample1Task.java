51e22036d1fdbb2e3044cc8a2e25d0f5eb1d4297
AnnieK7H <anjahalatyan@gmail.com>
2018-02-21 11:25:05 +0200

    first commit

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -11,13 +11,16 @@ public class Sample1Task {
     @Test
     public void goToHomepage() throws Exception {
         //define driver
-        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe"); //mandatory line to work
         WebDriver driver = new ChromeDriver();
 
         // TODO
         // go to https://kristinek.github.io/test-sample/index2.html
         // get title of page
         // get URL of current page
+        driver.get("https://kristinek.github.io/test-sample/index2.html"); //go to specific url
+        System.out.println(driver.getTitle()); //predefined functions (defined in selenium)
+        System.out.println(driver.getCurrentUrl());
 
         //Close browser
         driver.close();
