d22e7b64a95dad905fa1e9761f3ea03d7aa4421d
Andrejs_Ivanovs <dronson2@inbox.lv>
2018-02-21 11:30:19 +0200

    my commit

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -13,6 +13,11 @@ public class Sample1Task {
         //define driver
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://kristinek.github.io/test-sample/");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        Thread.sleep( 5000);
+
 
         // TODO
         // go to https://kristinek.github.io/test-sample/index2.html
