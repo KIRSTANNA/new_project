1ad93715a7024d943ee3e5d941f7f0690bad8f7f
TBallo <tballode@gmail.com>
2018-02-22 11:19:05 +0200

    Mt task 1

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,8 +3,14 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class Task1 {
     WebDriver driver;
@@ -23,25 +29,48 @@ public class Task1 {
     }
 
     @Test
-    public void errorOnText() {
+    public void errorOnText() throws Exception   {
+        driver.findElement(By.id("numb"));
+        driver.findElement(By.id("numb")).sendKeys("qwerty");
+        driver.findElement(By.tagName ("button")).click();
+        assertEquals("Please enter a number",driver.findElement(By.id("ch1_error")).getText());
+
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
     }
 
     @Test
     public void errorOnNumberTooSmall() {
+        driver.findElement(By.id("numb"));
+        driver.findElement(By.id("numb")).sendKeys("9");
+        driver.findElement(By.tagName("button")).click();
+        assertEquals("Number is too small",driver.findElement(By.id("ch1_error")).getText());
+
+
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
     }
 
     @Test
     public void errorOnNumberTooBig() {
+        driver.findElement(By.id("numb"));
+        driver.findElement(By.id("numb")).sendKeys("200");
+        driver.findElement(By.tagName("button")).click();
+        assertEquals("Number is too big",driver.findElement(By.id("ch1_error")).getText());
+
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
     }
 
     @Test
     public void correctSquareRootWithoutRemainder() {
+        driver.findElement(By.id("numb"));
+        driver.findElement(By.id("numb")).sendKeys("64");
+        driver.findElement(By.tagName("button")).click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 64 is 8.00",alert.getText());
+
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
