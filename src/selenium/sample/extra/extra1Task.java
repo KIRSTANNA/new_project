b3e8b2f0b1fb8e4b852b2aaafea9417314d78202
KrainevE <krainev3@gmail.com>
2018-02-22 12:01:14 +0200

    extra Task 1

--- src/selenium/sample/extra/extra1Task.java
+++ src/selenium/sample/extra/extra1Task.java
@@ -8,12 +8,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class extra1Task {
     WebDriver driver;
-    String base_url = "https://kristinek.github.io/test-sample/examples/act";
-    String new_url = "https://kristinek.github.io/test-sample/examples/link1";
+    String base_url = "https://kristinek.github.io/test-sample/examples/po";
+    String new_url = "https://kristinek.github.io/test-sample/examples/po1";
 
     // method which is being run before each test
     @Before
@@ -38,10 +38,15 @@ public class extra1Task {
     public void navigateBack() throws Exception {
 //        TODO
 //        open page with url "https://kristinek.github.io/test-sample/examples/po"
-//        click "More > " for the top left element
+//        click "More > " for the top left element a
 //        check that the url now "https://kristinek.github.io/test-sample/examples/po1"
 //        using driver navigation go back to "https://kristinek.github.io/test-sample/examples/po"
 //        check that the page now is "https://kristinek.github.io/test-sample/examples/po"
+        assertEquals(base_url, driver.getCurrentUrl());
+        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/p[2]/a")).click();
+        assertEquals(new_url, driver.getCurrentUrl());
+        driver.navigate().back();
+        assertEquals(base_url, driver.getCurrentUrl());
     }
 
     @Test
@@ -52,6 +57,13 @@ public class extra1Task {
 //        using driver navigation go back to "https://kristinek.github.io/test-sample/examples/po"
 //        using driver navigation go forward to "https://kristinek.github.io/test-sample/examples/po1"
 //        check that the page now is "https://kristinek.github.io/test-sample/examples/po1"
+        assertEquals(base_url, driver.getCurrentUrl());
+        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/p[2]/a")).click();
+        assertEquals(new_url, driver.getCurrentUrl());
+        driver.navigate().back();
+        assertEquals(base_url, driver.getCurrentUrl());
+        driver.navigate().forward();
+        assertEquals(new_url, driver.getCurrentUrl());
     }
 
     @Test
@@ -62,5 +74,10 @@ public class extra1Task {
 //        check that text "I am here!" is seen
 //        refresh page
 //        check that text "I am here!" is not seen
+        driver.get("https://kristinek.github.io/test-sample/examples/act");
+        driver.findElement(By.id("show_text")).click();
+        assertEquals("I am here!", driver.findElement(By.id("show_me")).getText());
+        driver.navigate().refresh();
+        assertFalse(driver.findElement(By.id("show_me")).isDisplayed());
     }
 }
