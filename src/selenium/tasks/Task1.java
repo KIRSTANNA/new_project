846b3f53ac539f469bd44f7042eb5b2d2bf3dba4
Student <kirstanna@inbox.lv>
2018-02-22 11:17:45 +0200

    increase sleep

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,8 +3,17 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
 
 public class Task1 {
     WebDriver driver;
@@ -26,18 +35,58 @@ public class Task1 {
     public void errorOnText() {
 //        TODO
 //        enter a text instead of a number, check that correct error is seen
+        WebElement inputField = driver.findElement(By.id("numb"));
+        inputField.sendKeys("tttt");
+
+        WebElement buttonSubmit = driver.findElement(By.tagName("button"));
+        buttonSubmit.click();
+
+        List<WebElement> errorMessages = driver.findElements(By.id("ch1_error"));
+        assertTrue(errorMessages.size()>0);
+        assertEquals("Please enter a number", errorMessages.get(0).getText());
     }
+
 
     @Test
     public void errorOnNumberTooSmall() {
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
+        WebElement inputField = driver.findElement(By.id("numb"));
+        inputField.sendKeys("23");
+
+        WebElement buttonSubmit = driver.findElement(By.tagName("button"));
+        buttonSubmit.click();
+
+        List<WebElement> errorMessages = driver.findElements(By.id("ch1_error"));
+
+        assertTrue(errorMessages.size()>0);
+        assertEquals("Number is too small", errorMessages.get(0).getText());
+
+        inputField.sendKeys("49");
+
+        buttonSubmit = driver.findElement(By.tagName("button"));
+        buttonSubmit.click();
+
+        errorMessages = driver.findElements(By.id("ch1_error"));
+
+        assertTrue(errorMessages.size()>0);
+        assertEquals("Number is too small", errorMessages.get(0).getText());
     }
 
     @Test
     public void errorOnNumberTooBig() {
 //        TODO
 //        enter number which is too big (above 100), check that correct error is seen
+        WebElement inputField = driver.findElement(By.id("numb"));
+        inputField.sendKeys("123");
+
+        WebElement buttonSubmit = driver.findElement(By.tagName("button"));
+        buttonSubmit.click();
+
+        List<WebElement> errorMessages = driver.findElements(By.id("ch1_error"));
+
+        assertTrue(errorMessages.size()>0);
+        assertEquals("Number is too big", errorMessages.get(0).getText());
     }
 
     @Test
@@ -45,6 +94,15 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+
+        WebElement inputField = driver.findElement(By.id("numb"));
+        inputField.sendKeys("81");
+
+        WebElement buttonSubmit = driver.findElement(By.tagName("button"));
+        buttonSubmit.click();
+
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 81 is 9.00", alert.getText());
     }
 
     @Test
@@ -52,5 +110,13 @@ public class Task1 {
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
+        WebElement inputField = driver.findElement(By.id("numb"));
+        inputField.sendKeys("80");
+
+        WebElement buttonSubmit = driver.findElement(By.tagName("button"));
+        buttonSubmit.click();
+
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 80 is 8.94", alert.getText());
     }
 }
