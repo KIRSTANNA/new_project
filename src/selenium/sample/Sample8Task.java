2d2363429b9a44f166d530ccd055cbc4bbff928d
KrainevE <krainev3@gmail.com>
2018-02-23 09:44:09 +0200

    new files

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -36,5 +36,14 @@ public class Sample8Task {
     public void styleChecks() throws Exception {
 //        check the background of top 2 sections
 //        check h1 element font-size, font-family
+        WebElement container1 = driver.findElement(By.className("w3-pale-red"));
+        System.out.println(container1.getCssValue("background-color"));
+        assertEquals("rgba(255, 221, 221, 1)",container1.getCssValue("background-color"));
+        WebElement container2 = driver.findElement(By.className("w3-pale-yellow"));
+        System.out.println(container2.getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 204, 1)", container2.getCssValue("background-color"));
+
+        assertEquals("64px", driver.findElement(By.className("w3-jumbo")).getCssValue("font-size"));
+
     }
 }
