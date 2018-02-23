49a47872538b4023233bf2dbe7dc450e762e1866
danviss <danilvysockii@gmail.com>
2018-02-23 09:43:39 +0200

    Task 1 Danils_Visockis. Completed

--- src/selenium/tasks/Task1.java
+++ src/selenium/tasks/Task1.java
@@ -3,18 +3,24 @@ package selenium.tasks;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 
 
 public class Task1 {
     WebDriver driver;
 
+
     @Before
     public void openPage(){
+
+
         assertTrue(true);
         String libWithDriversLocation =  System.getProperty("user.dir") + "\\lib\\";
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
@@ -50,6 +56,7 @@ public class Task1 {
 
 
 
+
 //        TODO
 //        enter number which is too small (below 50), check that correct error is seen
     }
@@ -68,13 +75,30 @@ public class Task1 {
 
     @Test
     public void correctSquareRootWithoutRemainder() {
+
+        driver.findElement(By.id("numb")).sendKeys("64");
+        driver.findElement(By.className("w3-btn")).click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 64 is 8.00", alert.getText());
+        alert.accept();
+
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 2 is square root of 4),
 //        then and press submit and check that correct no error is seen and check that square root is calculated correctly
+
     }
 
     @Test
     public void correctSquareRootWithRemainder() {
+
+
+        driver.findElement(By.id("numb")).sendKeys("80");
+        driver.findElement(By.className("w3-btn")).click();
+        Alert alert = driver.switchTo().alert();
+        assertEquals("Square root of 80 is 8.94", alert.getText());
+        alert.accept();
+
 //        TODO
 //        enter a number between 50 and 100 digit in the input (square root of which doesn't have a remainder, e.g. 1.732.. is square root of 3) and press submit,
 //        then check that correct no error is seen and check that square root is calculated correctly
